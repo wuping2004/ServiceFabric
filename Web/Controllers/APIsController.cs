e2e5aa87a46d9a10bbccbb65ed170adadba772ca
wuping2004@icloud.com <piw@microsoft.com>
2019-12-27 15:39:29 +0800

    first commit

---
+++ Web/Controllers/APIsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Produces("application/json")]
+    public class APIsController : Controller
+    {
+        // GET: api/APIs
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+
+   
+        // POST: api/APIs
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/APIs/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
